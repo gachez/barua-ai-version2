6c2ff2ddb92a6f97640233eb0be0b364d6dabdb8
brian-gacheru <briangach98@gmail.com>
2023-05-26 10:04:12 +0300

    add link to CTA button

--- src/pages/index.js
+++ src/pages/index.js
@@ -62,12 +62,12 @@ export default function SuccessPayment() {
             ))}
           </div>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <button
-              type="button"
+            <Link
+              href={"/app/auth"}
               className="rounded-full bg-indigo-600 px-4 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Open app
-            </button>
+            </Link>
           </div>
         </nav>
         <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
