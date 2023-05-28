054fabd73d1f62ce4a0ce8d7d4a64b07ea14acd8
brian-gacheru <briangach98@gmail.com>
2023-05-28 12:07:53 +0300

    hide upgrade buttons on smaller screens

--- src/components/NavBarTop.js
+++ src/components/NavBarTop.js
@@ -279,7 +279,7 @@ export default function NavBarTop(props) {
                     props.setOpen(true)
                   }}
                   type="button"
-                  className="inline-flex items-center gap-x-1.5 rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  className="hidden sm:inline-flex  items-center gap-x-1.5 rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                   >
                   <BanknotesIcon className="-ml-0.5 h-5 w-5" aria-hidden="true" />
                   Buy credits
@@ -295,7 +295,7 @@ export default function NavBarTop(props) {
                     startPayment()
                   }}
                   type="button"
-                  className="inline-flex items-center gap-x-1.5 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  className="hidden sm:inline-flex  items-center gap-x-1.5 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                   >
                     <BoltIcon className="-ml-0.5 h-5 w-5" aria-hidden="true" />
                     Upgrade
